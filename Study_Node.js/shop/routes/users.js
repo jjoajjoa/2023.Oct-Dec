762d13418bb565deb6a939941fb4d2b84649fe70
Arum <140360536+jjoajjoa@users.noreply.github.com>
2023-10-11 19:24:05 +0900

    Add: 231011

---
+++ Study_Node.js/shop/routes/users.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var router = express.Router();
+
+var db = require("../db");
+
+/*사용자 목록 페이지로*/
+router.get('/', function(req, res, next) {
+    res.render("index", {title:"사용자 목록", pageName:"users/list.ejs"})
+});
+
+//사용자 목록 json
+router.get("/list.json", function(req, res) {
+    const sql = "select * from users order by uname";
+    db.get().query(sql, function(err, rows) {
+        res.send(rows);
+    })
+});
+
+//로그인 페이지로 이동하는 라우터
+router.get("/login", function(req, res) {
+    res.render("index", {title:"로그인", pageName:"users/login.ejs"})
+});
+
+//로그인이 성공했는지안했는지 비번이맞는지안맞는지
+//폼에서 로그인 눌리면 그 데이터는 일로옴
+router.post("/login", function(req, res) {
+    const uid = req.body.uid;
+    const upass = req.body.upass;
+    const sql = "select * from users where uid = ?"
+    db.get().query(sql, [uid], function(err, rows) {
+        let result = '0';
+        if (rows[0]) {
+            if (rows[0].upass == upass) {
+                result = '1';
+            } else {
+                result = '2';
+            }
+        }
+        res.send(result);
+    });
+});
+
+//회원가입 페이지로 이동하는
+router.get("/insert", function(req, res) {
+    res.render("index", {title:"회원가입", pageName:"users/insert.ejs"})
+});
+
+//회원가입을 디비에 넣어줄 때
+router.post("/insert", function(req, res) {
+    const uid = req.body.uid;
+    const upass = req.body.upass;
+    const uname = req.body.uname;
+    const phone = req.body.phone;
+    const address1 = req.body.address1;
+    const address2 = req.body.address2;
+    console.log(uid, upass, uname, phone, address1, address2);
+
+    const sql = "insert into users (uid, upass, uname, phone, address1, address2) values(?, ?, ?, ?, ?, ?)"
+    db.get().query(sql, [uid, upass, uname, phone, address1, address2], function(err, rows) {
+        res.redirect("/users/login")
+    });
+})
+
+module.exports = router;
