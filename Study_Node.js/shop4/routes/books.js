acc3e51770999d40fcc9633b6bd7a1313cc17be4
Arum <140360536+jjoajjoa@users.noreply.github.com>
2023-10-16 17:48:36 +0900

    Add: 231016

---
+++ Study_Node.js/shop4/routes/books.js
@@ -0,0 +1,140 @@
+var express = require('express');
+var router = express.Router();
+var db = require("../db");
+var multer = require("multer");
+
+
+//도서 이미지파일 업로드함수
+var upload = multer({
+    storage:multer.diskStorage({
+        destination:(req, file, done) => {
+            done(null, './public/upload/book')
+        },
+        filename:(req, file, done) => {
+            var fileName=Date.now() + ".jpg";
+            done(null, fileName);
+        }
+    })
+});
+
+/* 도서검색 페이지 출력하기 위한 라우터 */
+router.get('/', function(req, res, next) {
+    res.render('index', {title:"도서검색", pageName:"books/search.ejs"});
+});
+
+//도서검색결과저장
+router.post('/search/insert', function(req, res) {
+    const title = req.body.title;
+    const authors = req.body.authors;
+    const price = req.body.price;
+    const publisher = req.body.publisher;
+    const image = req.body.thumbnail;
+    const contents = req.body.contents;
+    const isbn = req.body.isbn;
+    // console.log(title, authors, price, publisher, image, contents);
+
+    //처음에 먼저 isbn 정보가 있는지 먼저 체크하고 없으면 추가하기
+    const sql1 = "select * from books where isbn=?"
+    db.get().query(sql1, [isbn], function(err, rows) {
+        if (rows.length > 0) {
+            res.send("1"); //isbn 값이 있을때
+        } else {
+            const sql = "insert into books(title, authors, price, publisher, image, contents, isbn) values(?, ?, ?, ?, ?, ?, ?)";
+            db.get().query(sql, [title, authors, price, publisher, image, contents, isbn], function(err, rows) {
+                if(err) console.log("!!!!도서저장오류: ", err);
+                res.send("0"); 
+            }); 
+        }
+    });
+});
+
+//도서목록 JSON 가져오는 
+router.get("/list.json", function(req, res) {
+    const page = req.query.page;
+    const start = (parseInt(page)-1) *5;
+    const key = req.query.key;
+    const query = '%' + req.query.query + '%';
+    const sql = 'select * from books where ' + key + ' like ? order by bid desc limit ?, 5';
+    db.get().query(sql, [query, start], function(err, rows) {
+        if(err) console.log("!!!!!도서목록jon: ", err);
+        res.send(rows);
+    });
+});
+
+//책 간직함으로 이동하는
+router.get("/list", function(req, res) {
+    res.render("index", {title:"도서목록", pageName:"books/list.ejs"});
+});
+
+//보관 책 갯수를 확인하는
+router.get("/count", function(req, res) {
+    const key = req.query.key;
+    const query = '%' + req.query.query + '%';
+    const sql = 'select count(*) total from books where ' + key + ' like ? ';
+    db.get().query(sql, [query], function(err, rows) {
+        res.send(rows[0]);
+    });
+});
+
+//도서 삭제하는 //프론트쪽에서 삭제할 아이디를 이쪽으로 보내줌
+router.post("/delete", function(req, res) {
+    const bid = req.body.bid;
+    const sql = "delete from books where bid = ?";
+    db.get().query(sql, [bid], function(err, rows) {
+        if(err) console.log("!!!!도서삭제: ", err);
+        res.sendStatus(200);
+    });
+});
+
+//도서정보 페이지 이동
+router.get("/read", function(req, res) {
+    const bid = req.query.bid;
+    //포스트에서 보내준 bid값이 여기로 넘어옴
+    const sql = "select *, format(price, 0) fmtprice, date_format(regdate, '%Y-%m-%d') fmtdate from books where bid = ?";
+    db.get().query(sql, [bid], function(err, rows) {
+        if(err) console.log("!!!!도서정보: ", err);
+        res.render("index", {title:"도서정보", pageName:"books/read.ejs", book:rows[0]});
+    });
+});
+
+//도서정보 수정하는 페이지로 이동하는
+router.get("/update", function(req, res) {
+    const bid = req.query.bid;
+    const sql = "select *, format(price, 0) fmtprice, date_format(regdate, '%Y-%m-%d') fmtdate from books where bid = ?";
+    db.get().query(sql, [bid], function(err, rows) {
+        if(err) console.log("!!!!도서정보: ", err);
+        res.render("index", {title:"정보수정", pageName:"books/update.ejs", book:rows[0]});
+    });
+});
+
+//도서정보 수정하는 DB
+router.post("/update", function(req, res) {
+    const bid = req.body.bid;
+    const title = req.body.title;
+    const price = req.body.price;
+    const authors = req.body.authors;
+    const publisher = req.body.publisher;
+    const contents = req.body.contents;
+    //console.log(bid, title, price, authors, publisher, contents);
+    const sql = "update books set title=?, price=?, authors=?, publisher=?, contents=? where bid=?";
+    db.get().query(sql, [title, price, authors, publisher, contents, bid], function(err, rows) {
+        if(err) console.log("!!!!수정오류: ", err);
+        res.redirect("/books/read?bid=" + bid);
+    })
+})
+
+//도서썸네일 업로드함수
+router.post("/upload", upload.single("file"), function(req, res) {
+    if(req.file) {
+        const bid = req.body.bid;
+        //console.log("!!!!파일이름: ", req.file.filename, "  ", bid);
+        const image = "/upload/book/" + req.file.filename;
+        const sql = "update books set image=? where bid=?"
+        db.get().query(sql, [image, bid], function(err, rows) {
+            if(err) console.log("!!!!이미지업로드오류: ", err);
+            res.redirect("/books/read?bid=" + bid);
+        });
+    }
+});
+
+module.exports = router;
